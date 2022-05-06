39f02ef6d8c3de7e963dc9cad5fe2a6f8814dfd7
cameronjohnson30 <cameron_johnson91@icloud.com>
2022-05-06 14:26:46 -0700

    test for clearing out words

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -31,4 +31,15 @@ describe('#word') do
       expect(word).to(eq(word2))
     end
   end
+  describe('.clear') do
+    it('clears all words') do
+      word = Word.new('Bootswith', nil)
+      word.save()
+      word2 = Word.new('TheFur', nil)
+      word2.save()
+      Word.clear
+      expect(Word.all).to(eq([]))
+    end
+  end
+
 end
