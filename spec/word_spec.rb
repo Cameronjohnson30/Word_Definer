aee2189db849ed93581a3f5ece865f030e4088e8
cameronjohnson30 <cameron_johnson91@icloud.com>
2022-05-06 14:23:20 -0700

    add save test and same word test

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -13,4 +13,22 @@ describe('#word') do
       expect(Word.all).to(eq([]))
     end
   end
+
+  describe('#save') do 
+    it('saves a word') do 
+      word = Word.new('apple', nil)
+      word.save()
+      word2 = Word.new('bottom', nil)
+      word2.save()
+      expect(Word.all).to(eq([word, word2]))
+    end
+  end
+
+  describe('#==') do
+    it("is the same word if it has the same attributes as another word") do
+      word = Word.new('Jeans', nil)
+      word2 = Word.new('Jeans', nil)
+      expect(word).to(eq(word2))
+    end
+  end
 end
