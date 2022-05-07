5752b10b188b23bc8b0c453bd39eef79c0f40cc5
cameronjohnson30 <cameron_johnson91@icloud.com>
2022-05-07 01:26:02 -0700

    add get, patch, delete for /words/:id

--- app.rb
+++ app.rb
@@ -27,6 +27,27 @@ get('/words/new') do
 end
 
 get('/words/:id') do 
-  @words = Word.find(params[:id].to_i())
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+end
+
+
+get('/words/:id/edit') do
+  @word = Word.find(params[:id].to_i())
   erb(:edit_word)
 end
+
+patch('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.update(params[:word_name])
+  @words = Word.all
+  erb(:words)
+end
+
+delete('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.delete()
+  @words = Word.all
+  erb(:words)
+end
+
