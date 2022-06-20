0378b3c1f644767effff5528c3d93aec89b0edbb
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-06-20 03:05:51 -0700

    BUGFIX grammer corresctions and added final tests passing

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -1,11 +1,9 @@
-require 'rspec'
-require 'definition'
-require 'word'
-require 'pry'
+require('rspec')
+require('definition')
+require('word')
+require('pry')
 
-
-describe '#Definition' do
-
+describe ('Definition') do
   before(:each) do
     Word.clear()
     Definition.clear()
@@ -13,14 +11,21 @@ describe '#Definition' do
     @word.save()
   end
 
-  describe('#==') do
-  it("is the same definition if it has the same attributes as another definition") do
-    definition = Definition.new("lowlow", @word.id, nil)
-    definition2 = Definition.new("lowlow", @word.id, nil)
-    expect(definition).to(eq(definition2))
+  describe('#word') do
+    it("finds the word a definition belongs to") do
+      definition = Definition.new("Peacock", @word.id, nil)
+      definition.save()
+      expect(definition.word()).to(eq(@word))
     end
   end
 
+  describe('#==') do
+    it("is the same definition if it has the same attributes as another definition") do
+      definition = Definition.new("lowlow", @word.id, nil)
+      definition2 = Definition.new("lowlow", @word.id, nil)
+      expect(definition).to(eq(definition2))
+    end
+  end
 
   describe('.all') do
     it("returns a list of all definitions") do
@@ -62,7 +67,7 @@ describe '#Definition' do
   end
 
   describe('#update') do
-    it("updates an definition by id") do
+    it("updates a definition by id") do
       definition = Definition.new("Normal", @word.id, nil)
       definition.save()
       definition.update("testing", @word.id)
@@ -70,14 +75,34 @@ describe '#Definition' do
     end
   end
 
-  describe('#delete') do
-    it("deletes an definition by id") do
+  describe("#delete") do
+    it("deletes a definition by id") do
       definition = Definition.new("almost", @word.id, nil)
       definition.save()
       definition2 = Definition.new("done", @word.id, nil)
       definition2.save()
       definition.delete()
       expect(Definition.all).to(eq([definition2]))
+    end
+  end
+
+  describe('.find_by_word') do
+    it("finds definition of a word") do
+      word2 = Word.new("Yellow", nil)
+      word2.save()
+      definition = Definition.new("dragon", @word.id, nil)
+      definition.save()
+      definition2 = Definition.new("monkey", word2.id, nil)
+      definition2.save()
+      expect(Definition.find_by_word(word2.id)).to(eq([definition2]))
+    end
+  end
+
+  describe('#word') do
+    it("finds the word the definition belongs to") do
+      definition = Definition.new("penguin", @word.id, nil)
+      definition.save()
+      expect(definition.word()).to(eq(@word))
     end
   end
 end
